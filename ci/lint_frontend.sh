5096afa7cd6995104394b0f8cf6ccab134a08a82
andreparo <andrea.parodi.mi@gmail.com>
2026-04-15 16:13:48 +0200

    Trying to further optimize Lint

--- ci/lint_frontend.sh
+++ ci/lint_frontend.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-repo_root="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+repo_root="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd "$repo_root"
 
 bash ci/lint/check_frontend_with_eslint.sh
