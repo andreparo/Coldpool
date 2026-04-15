25e9485335fa2990080644739286200c50429e08
andreparo <andrea.parodi.mi@gmail.com>
2026-04-15 19:25:25 +0200

    fixed missing __init__ files

--- tests/unit/test_artifact.py
+++ tests/unit/test_artifact.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from datetime import datetime
 
-from coldpool_server.artifact import Artifact
+from coldpool_server.artifact import Artifact 
 from coldpool_server.artifact_version import ArtifactVersion
 
 
