3768e118564f28d46dd2d3c203155822c9914b66
andreparo <andrea.parodi.mi@gmail.com>
2026-04-15 13:10:11 +0200

    Testing docker

--- ci/versioning.sh
+++ ci/versioning.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 repo_root="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd "$repo_root"
 
+git config --global --add safe.directory "$repo_root"
+
 python3 ci/versioning/verify_changelog.py
 python3 ci/versioning/compute_version.py > ci_version.env
-
 cat ci_version.env
