13857018de686ff72c8584dafc98099fa73be256
andreparo <andrea.parodi.mi@gmail.com>
2026-04-16 15:30:30 +0200

    Fixing build stage

--- ci/docker/setup/prepare_dependency_image.sh
+++ ci/docker/setup/prepare_dependency_image.sh
@@ -6,12 +6,8 @@ cd "$repo_root"
 
 dependency_image="${1:?Usage: bash ci/docker/setup/prepare_dependency_image.sh <dependency_image>}"
 
-if docker image inspect "$dependency_image" >/dev/null 2>&1; then
-    echo "Dependency image already exists: $dependency_image"
-    exit 0
-fi
-
 docker build \
+    --pull \
     -t "$dependency_image" \
     -f ci/docker/dependency-image/Dockerfile \
     .
